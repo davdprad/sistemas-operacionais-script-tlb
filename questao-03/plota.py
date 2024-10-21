ee70c7f63648c366301a37019b799ea3fecb9aca
Lucas Eduardo <lucaseduumoura@gmail.com>
2024-10-20 23:43:46 -0400

    feat: questao-03

--- questao-03/plota.py
+++ questao-03/plota.py
@@ -1,5 +1,27 @@
-import sys
+# import sys
+# import matplotlib.pyplot as plt
+
+# plotprofile se mostrou antigo, então foi substituido no código
+# plt.plotfile(sys.argv[1], cols=(0, 1), names=["Numero de Paginas", "Tempo"], delimiter=" ")
+# plt.show()
+
+import numpy as np
 import matplotlib.pyplot as plt
 
-plt.plotfile(sys.argv[1], cols=(0, 1), names=["Numero de Paginas", "Tempo"], delimiter=" ")
+data = np.loadtxt('tlb_resultados.txt', skiprows=1)
+
+numero_paginas = data[:, 0]
+tempo_acesso = data[:, 1]
+
+plt.figure(figsize=(10, 6))
+plt.plot(numero_paginas, tempo_acesso, marker='o', linestyle='-', color='b', label='Tempo por Acesso')
+
+plt.title("Tempo Médio por Acesso vs. Número de Páginas")
+plt.xlabel("Número de Páginas")
+plt.ylabel("Tempo Médio por Acesso (microssegundos)")
+
+plt.legend()
+
+
+plt.grid(True)
 plt.show()
